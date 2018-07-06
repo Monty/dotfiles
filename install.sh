cf1c2f23101fce6cf2675bde8ac4784384ec0645
Monty Williams <monty.williams@gmail.com>
2018-07-05 22:51:25 -0700

    Handle possibility that .config already exists

--- install.sh
+++ install.sh
@@ -1,16 +1,39 @@
 #!/usr/bin/env bash
 
-DOTDIR=$(pwd)
-echo "==> Setting up $DOTDIR"
-cd $HOME
+DOTDIR="${HOME}/dotfiles"
+HOMEDIR="${HOME}"
+printf "==> Setting up links to \n${DOTDIR} in \n${HOMEDIR}\n"
+cd ${HOMEDIR}
 
-for file in $(ls $DOTDIR); do
-    if [[ ${file} =~ \.sh$ ]] || [[ ${file} =~ \.txt$ ]] ||
-        [[ ${file} =~ \.rdoc$ ]]; then
-        echo "==> Skipping ${file}"
-    else
-        echo "==> Processing ${file}"
-        rm -rf .${file}
-        ln -s $DOTDIR/${file} .${file}
-    fi
+echo ""
+
+for file in $(ls ${DOTDIR}); do
+	if [[ ${file} =~ \.sh$ ]] || [[ ${file} =~ \.txt$ ]] ||
+		[[ ${file} =~ \.rdoc$ ]] || [[ ${file} =~ \.dir$ ]]; then
+		echo "==> Skipping ${file}"
+	else
+		echo "==> Processing ${file}"
+		echo "rm -rf .${file}"
+		echo "ln -s ${DOTDIR}/${file} .${file}"
+	fi
+done
+
+echo ""
+
+for LINKDIR in $(ls -d ${DOTDIR}/*.dir); do
+	TARGETDIR="${HOMEDIR}/.$(basename ${LINKDIR} .dir)"
+	mkdir -p ${TARGETDIR}
+	printf "==> Setting up links to \n${LINKDIR} in \n${TARGETDIR}\n"
+    cd ${TARGETDIR}
+    pwd
+	for file in $(ls ${LINKDIR}); do
+		if [[ ${file} =~ \.sh$ ]] || [[ ${file} =~ \.txt$ ]] ||
+			[[ ${file} =~ \.rdoc$ ]] || [[ ${file} =~ \.dir$ ]]; then
+			echo "==> Skipping ${file}"
+		else
+			echo "==> Processing ${file}"
+			echo "rm -rf ${file}"
+			echo "ln -s ${LINKDIR}/${file} ${file}"
+		fi
+	done
 done
