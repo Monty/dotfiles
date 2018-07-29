5c7b9fbfe6f24e7ed2b073c271a90ed1398c1158
Monty Williams <monty.williams@gmail.com>
2018-07-29 13:53:44 -0700

    fix tabs

--- install.sh
+++ install.sh
@@ -6,26 +6,26 @@ printf "==> Setting up links to \n${DOTDIR} in \n${HOMEDIR}\n\n"
 cd ${HOMEDIR}
 
 for file in $(ls ${DOTDIR}); do
-	if [[ ${file} =~ \.sh$ ]] || [[ ${file} =~ \.txt$ ]] ||
-		[[ ${file} =~ \.rdoc$ ]] || [[ ${file} =~ \.dir$ ]]; then
-		printf "==> Skipping ${file}\n"
-	else
-		printf "==> Processing ${file}\n"
-		rm -rf .${file}
-		ln -s ${DOTDIR}/${file} .${file}
-	fi
+    if [[ ${file} =~ \.sh$ ]] || [[ ${file} =~ \.txt$ ]] ||
+        [[ ${file} =~ \.rdoc$ ]] || [[ ${file} =~ \.dir$ ]]; then
+        printf "==> Skipping ${file}\n"
+    else
+        printf "==> Processing ${file}\n"
+        rm -rf .${file}
+        ln -s ${DOTDIR}/${file} .${file}
+    fi
 done
 
 echo ""
 
 for LINKDIR in $(ls -d ${DOTDIR}/*.dir); do
-	TARGETDIR="${HOMEDIR}/.$(basename ${LINKDIR} .dir)"
-	mkdir -p ${TARGETDIR}
-	printf "==> Setting up links to \n${LINKDIR} in \n${TARGETDIR}\n"
-	cd ${TARGETDIR}
-	for file in $(ls ${LINKDIR}); do
-		printf "==> Processing ${file}\n"
-		rm -rf ${file}
-		ln -s ${LINKDIR}/${file} ${file}
-	done
+    TARGETDIR="${HOMEDIR}/.$(basename ${LINKDIR} .dir)"
+    mkdir -p ${TARGETDIR}
+    printf "==> Setting up links to \n${LINKDIR} in \n${TARGETDIR}\n"
+    cd ${TARGETDIR}
+    for file in $(ls ${LINKDIR}); do
+        printf "==> Processing ${file}\n"
+        rm -rf ${file}
+        ln -s ${LINKDIR}/${file} ${file}
+    done
 done
