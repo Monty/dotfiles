3c5820c7481542bdda470706fafd62b10b70a38e
Monty Williams <monty.williams@gmail.com>
2020-12-18 20:48:09 -0800

    Make git quiet. Add vim-commentary.git

--- clone_vim_bundles.sh
+++ clone_vim_bundles.sh
@@ -3,13 +3,15 @@
 DIRNAME=$(dirname "$0")
 cd $DIRNAME
 
-printf "==> Vim bundles: ScrollColors vim-fugitive vim-matchit vim-shfmt vim-smarttabs vim-unimpaired\n"
+printf "==> Vim bundles: \n"
+printf "==> ScrollColors vim-fugitive vim-matchit vim-shfmt vim-smarttabs vim-unimpaired\n"
 
 cd vim/bundle
 
-git clone https://github.com/vim-scripts/ScrollColors.git
-git clone https://github.com/tpope/vim-fugitive.git
-git clone https://github.com/adelarsq/vim-matchit.git
-git clone https://github.com/dpc/vim-smarttabs.git
-git clone https://github.com/z0mbix/vim-shfmt.git
-git clone https://github.com/tpope/vim-unimpaired.git
+git clone -q https://github.com/vim-scripts/ScrollColors.git
+git clone -q https://github.com/tpope/vim-commentary.git
+git clone -q https://github.com/tpope/vim-fugitive.git
+git clone -q https://github.com/adelarsq/vim-matchit.git
+git clone -q https://github.com/dpc/vim-smarttabs.git
+git clone -q https://github.com/z0mbix/vim-shfmt.git
+git clone -q https://github.com/tpope/vim-unimpaired.git
