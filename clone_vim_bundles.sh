cbfa5bd26b018f871e1793548366af3dd3c436da
Monty Williams <monty.williams@gmail.com>
2020-12-19 18:27:59 -0800

    Update clone_vim_bundles.sh

--- clone_vim_bundles.sh
+++ clone_vim_bundles.sh
@@ -1,17 +1,22 @@
 #!/usr/bin/env bash
 
 DIRNAME=$(dirname "$0")
-cd $DIRNAME
+cd $DIRNAME/vim/bundle
 
-printf "==> Vim bundles: \n"
-printf "==> ScrollColors vim-fugitive vim-matchit vim-shfmt vim-smarttabs vim-unimpaired\n"
+printf "==> Cloning vim bundles:\n"
 
-cd vim/bundle
-
-git clone -q https://github.com/vim-scripts/ScrollColors.git
-git clone -q https://github.com/tpope/vim-commentary.git
-git clone -q https://github.com/tpope/vim-fugitive.git
-git clone -q https://github.com/adelarsq/vim-matchit.git
-git clone -q https://github.com/dpc/vim-smarttabs.git
-git clone -q https://github.com/z0mbix/vim-shfmt.git
-git clone -q https://github.com/tpope/vim-unimpaired.git
+for repo in https://github.com/vim-scripts/ScrollColors.git \
+    https://github.com/tpope/vim-commentary.git \
+    https://github.com/tpope/vim-fugitive.git \
+    https://github.com/adelarsq/vim-matchit.git \
+    https://github.com/dpc/vim-smarttabs.git \
+    https://github.com/z0mbix/vim-shfmt.git \
+    https://github.com/tpope/vim-unimpaired.git; do
+    directory="$(basename -s .git $repo)"
+    if [ -e "$directory" ]; then
+        printf "$directory already exists.\n"
+    else
+        printf "Cloning $repo into $directory\n"
+        git clone -q "$repo" -o "$directory"
+    fi
+done
