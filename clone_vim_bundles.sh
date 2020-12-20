8f092abe31f81e426650dcf170e308f100995ca4
Monty Williams <monty.williams@gmail.com>
2020-12-20 11:50:06 -0800

    Fix clone_vim_bundles

--- clone_vim_bundles.sh
+++ clone_vim_bundles.sh
@@ -1,17 +1,33 @@
 #!/usr/bin/env bash
 
 DIRNAME=$(dirname "$0")
-cd $DIRNAME
+cd $DIRNAME/vim/bundle
 
-printf "==> Vim bundles: \n"
-printf "==> ScrollColors vim-fugitive vim-matchit vim-shfmt vim-smarttabs vim-unimpaired\n"
+printf "==> Cloning vim bundles:\n"
 
-cd vim/bundle
+for repo in https://github.com/vim-scripts/ScrollColors.git \
+    https://github.com/tpope/vim-commentary.git \
+    https://github.com/tpope/vim-fugitive.git \
+    https://github.com/adelarsq/vim-matchit.git \
+    https://github.com/dpc/vim-smarttabs.git \
+    https://github.com/z0mbix/vim-shfmt.git \
+    https://github.com/tpope/vim-unimpaired.git; do
+    directory="$(basename -s .git $repo)"
+    if [ -e "$directory" ]; then
+        printf "$directory already exists.\n"
+    else
+        printf "Cloning $repo into $directory\n"
+        git clone -q "$repo"
+    fi
+done
 
-git clone -q https://github.com/vim-scripts/ScrollColors.git
-git clone -q https://github.com/tpope/vim-commentary.git
-git clone -q https://github.com/tpope/vim-fugitive.git
-git clone -q https://github.com/adelarsq/vim-matchit.git
-git clone -q https://github.com/dpc/vim-smarttabs.git
-git clone -q https://github.com/z0mbix/vim-shfmt.git
-git clone -q https://github.com/tpope/vim-unimpaired.git
+# Possible bundle candidates
+# ScrollColors      Add :COLORSCROLL
+# vim-commentary    Comment/Uncomment
+# vim-fugitive      Add :git
+# vim-matchit       Extended matching for the % operator
+# vim-polyglot      A collection of language packs
+# vim-sensible      Reasonabe settings
+# vim-shfmt         Format shell scripts with :Shfmt -i 4 -s
+# vim-smarttabs     Indent with tabs, align with spaces
+# vim-unimpaired    Pairs of handy bracket mappings
