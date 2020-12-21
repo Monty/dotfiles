ecb2185140defa810560e12560c0737cb8d083c4
Monty Williams <monty.williams@gmail.com>
2020-12-20 19:00:35 -0800

    Update clone_vim_bundles.sh

--- clone_vim_bundles.sh
+++ clone_vim_bundles.sh
@@ -9,8 +9,8 @@ for repo in https://github.com/vim-scripts/ScrollColors.git \
     https://github.com/tpope/vim-commentary.git \
     https://github.com/tpope/vim-fugitive.git \
     https://github.com/adelarsq/vim-matchit.git \
-    https://github.com/dpc/vim-smarttabs.git \
     https://github.com/z0mbix/vim-shfmt.git \
+    https://github.com/dpc/vim-smarttabs.git \
     https://github.com/tpope/vim-unimpaired.git; do
     directory="$(basename -s .git $repo)"
     if [ -e "$directory" ]; then
@@ -21,13 +21,15 @@ for repo in https://github.com/vim-scripts/ScrollColors.git \
     fi
 done
 
-# Possible bundle candidates
+# Reasonable bundle candidates
 # ScrollColors      Add :COLORSCROLL
 # vim-commentary    Comment/Uncomment
 # vim-fugitive      Add :git
 # vim-matchit       Extended matching for the % operator
-# vim-polyglot      A collection of language packs
-# vim-sensible      Reasonabe settings
 # vim-shfmt         Format shell scripts with :Shfmt -i 4 -s
 # vim-smarttabs     Indent with tabs, align with spaces
 # vim-unimpaired    Pairs of handy bracket mappings
+
+# Possible future bundle candidates
+# vim-polyglot      A collection of language packs      https://github.com/sheerun/vim-polyglot.git
+# vim-sensible      Reasonable settings                  https://github.com/tpope/vim-sensible.git
