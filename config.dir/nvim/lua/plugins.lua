b8972c2d756aed7119cb095d5514546926e30704
Monty Williams <monty.williams@gmail.com>
2023-04-14 08:49:45 -0700

    remove extra blank line

--- config.dir/nvim/lua/plugins.lua
+++ config.dir/nvim/lua/plugins.lua
@@ -1,7 +1,6 @@
 return require('packer').startup(function(use)
         use 'wbthomason/packer.nvim'
-        use 'williamboman/mason.nvim'   
+        use 'williamboman/mason.nvim'
         use 'williamboman/mason-lspconfig.nvim'
-        use 'neovim/nvim-lspconfig'  
- 
+        use 'neovim/nvim-lspconfig'
 end)
