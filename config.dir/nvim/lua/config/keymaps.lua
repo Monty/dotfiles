47178ccc273072875aecd5baea72ef96ea7d2ab7
Monty Williams <monty.williams@gmail.com>
2023-04-30 22:58:06 -0700

    Update to latest version of modern-neovim

--- config.dir/nvim/lua/config/keymaps.lua
+++ config.dir/nvim/lua/config/keymaps.lua
@@ -10,6 +10,10 @@ keymap("n", "N", "Nzzzv")
 keymap("n", "g,", "g,zvzz")
 keymap("n", "g;", "g;zvzz")
 
+-- Scrolling
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
+
 -- Paste
 keymap("n", "]p", "o<Esc>p", { desc = "Paste below" })
 keymap("n", "]P", "O<Esc>p", { desc = "Paste above" })
@@ -49,5 +53,14 @@ keymap("n", "<S-Left>", "<cmd>vertical resize -2<CR>")
 keymap("n", "<S-Right>", "<cmd>vertical resize +2<CR>")
 
 -- Insert blank line
-keymap("n", "]<Space>", "o<Esc>", default_opts)
-keymap("n", "[<Space>", "O<Esc>", default_opts)
+keymap("n", "]<Space>", "o<Esc>")
+keymap("n", "[<Space>", "O<Esc>")
+
+-- Auto indent
+keymap("n", "i", function()
+  if #vim.fn.getline "." == 0 then
+    return [["_cc]]
+  else
+    return "i"
+  end
+end, { expr = true })
