7b8857cfc2b6279ee3aee2dcab1d7cf1ce41d7ff
Monty Williams <monty.williams@gmail.com>
2024-04-01 18:20:12 -0700

    Cleaning up shellcheck issues

--- install.sh
+++ install.sh
@@ -1,37 +1,41 @@
 #!/usr/bin/env bash
 
 DOTDIR="${HOME}/dotfiles"
-printf "==> Setting up links to ${DOTDIR} \n    in ${HOME}\n\n"
-cd ${HOME} || exit
+printf "# Setting up links to %s in %s\n" "${DOTDIR}" "${HOME}"
+cd "${HOME}" || exit
 
+# .bash_aliases and .zsh_aliases should both link to dotfiles/aliases
 printf "==> Processing aliases\n"
 rm -f .bash_aliases .zsh_aliases
 printf "    Linking to .bash_aliases\n"
-ln -s ${DOTDIR}/aliases .bash_aliases
+ln -s "${DOTDIR}"/aliases .bash_aliases
 printf "    Linking to .zsh_aliases\n"
-ln -s ${DOTDIR}/aliases .zsh_aliases
+ln -s "${DOTDIR}"/aliases .zsh_aliases
 
-for file in $(ls ${DOTDIR}); do
-    if [[ ${file} =~ aliases$ ]] ||
-        [[ ${file} =~ \. ]]; then
-        printf "==> Skipping ${file}\n"
+for file in $(ls "${DOTDIR}"); do
+    if [[ ${file} =~ aliases$ ]] ||               # Already proceesed above
+        [[ ${file} =~ \. ]]; then                 # Skip files with . in filename
+        printf "==> Skipping %s\n" "${file}"
     else
-        printf "==> Linking ${file}\n"
-        rm -f .${file}
-        ln -s ${DOTDIR}/${file} .${file}
+        printf "==> Linking %s\n" "${file}"
+        rm -f ."${file}"
+        ln -s "${DOTDIR}"/"${file}" ."${file}"    # Add leading . and link
     fi
 done
 
 echo ""
 
-for LINKDIR in $(ls -d ${DOTDIR}/*.dir); do
-    TARGETDIR="${HOME}/.$(basename ${LINKDIR} .dir)"
-    mkdir -p ${TARGETDIR}
-    printf "==> Setting up links to ${LINKDIR} \n    in ${TARGETDIR}\n"
-    cd ${TARGETDIR}
-    for file in $(ls ${LINKDIR}); do
-        printf "==> Linking ${file}\n"
-        rm -f ${file}
-        ln -s ${LINKDIR}/${file} ${file}
+# By convention.link all files in any directories that end in .dir
+# But strip the .dir first so .config links to dotfiles/config.dir
+for LINKDIR in $(ls -d "${DOTDIR}"/*.dir); do
+    TARGETDIR="${HOME}/.${LINKDIR##*/}"
+    TARGETDIR="${TARGETDIR%.dir}"
+    mkdir -p "${TARGETDIR}"
+    printf "# Setting up links to %s in %s\n" "${LINKDIR}" "${TARGETDIR}"
+    cd "${TARGETDIR}" || exit
+    for file in $(ls "${LINKDIR}"); do
+        printf "==> Linking %s\n" "${file}"
+        rm -f "${file}"
+        ln -s "${LINKDIR}"/"${file}" "${file}"
     done
 done
