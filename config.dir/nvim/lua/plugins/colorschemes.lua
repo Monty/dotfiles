fe03a91e713b7153222891749eb39e7f58897f81
Monty Williams <monty.williams@gmail.com>
2024-04-27 20:17:47 -0700

    Add Catppuccin

--- config.dir/nvim/lua/plugins/colorschemes.lua
+++ config.dir/nvim/lua/plugins/colorschemes.lua
@@ -1,4 +1,6 @@
 return {
   -- add enfocado
   { "wuelnerdotexe/vim-enfocado", lazy = false, name = "enfocado" },
+  -- add catppuccin
+  { "catppuccin/nvim", lazy = false, name = "catppuccin" },
 }
