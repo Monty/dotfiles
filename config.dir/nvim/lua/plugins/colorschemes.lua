710190ac9798a45411863952e5151b50d554cec7
Monty Williams <monty.williams@gmail.com>
2024-04-29 10:28:40 -0700

    default nvim colorscheme to catppuccin-mocha

--- config.dir/nvim/lua/plugins/colorschemes.lua
+++ config.dir/nvim/lua/plugins/colorschemes.lua
@@ -3,4 +3,10 @@ return {
   { "wuelnerdotexe/vim-enfocado", lazy = false, name = "enfocado" },
   -- add catppuccin
   { "catppuccin/nvim", lazy = false, name = "catppuccin" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-mocha",
+    },
+  },
 }
