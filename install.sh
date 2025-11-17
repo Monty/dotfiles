083ec41102d94030031adcce5ee29f5984554c52
Monty Williams <monty.williams@gmail.com>
2025-11-17 13:20:33 -0800

    Add a --dry-run|-d option

--- install.sh
+++ install.sh
@@ -1,48 +1,66 @@
 #!/usr/bin/env bash
-# Create appropriate links in ~ to files in ~/dotfiles
+# Create links in ~, ~/.config, and ~/.local/share to files in ~/dotfiles
+
+# Prevent cascading or pipe failures
+set -euo pipefail
+
+# Check for dry run arguments
+DRY_RUN=false
+if [[ ${1-} =~ ^(--dry-run|-d)$ ]]; then
+    DRY_RUN=true
+    printf "==> Starting dry run...\n"
+fi
+
+# Function to execute command unless in dry-run mode
+maybe_run() {
+    if [[ $DRY_RUN == false ]]; then
+        "$@"
+    fi
+}
 
 DOTDIR="${HOME}/dotfiles"
 cd "${HOME}" || exit
 
 printf "# Creating links to %s in %s\n" "${DOTDIR#"$HOME"/}" "${HOME}"
+
 # Skip files with . in filename
 for file in $(eza -1 "${DOTDIR}" | rg -v "aliases$" | rg '\.'); do
-    printf "==> Skipping %s\n" "${file}"
+    printf "==> Ignoring %s\n" "${file}"
 done
 
 # Link files without . in filename
 for file in $(eza -1 "${DOTDIR}" | rg -v "aliases$" | rg -v '\.'); do
     printf "==> Linking .%s\n" "${file}"
-    rm -f ."${file}"
-    ln -s "${DOTDIR}"/"${file}" ."${file}" # Add a leading . and link
+    maybe_run rm -f ."${file}"
+    maybe_run ln -s "${DOTDIR}"/"${file}" ."${file}" # Add a leading . and link
 done
 
 # .bash_aliases and .zsh_aliases should both link to dotfiles/aliases
 printf "# Creating links to dotfiles/aliases in %s\n" "${HOME}"
 printf "==> Linking .bash_aliases\n"
-ln -sf "${DOTDIR}"/aliases .bash_aliases
+maybe_run ln -sf "${DOTDIR}"/aliases .bash_aliases
 printf "==> Linking .zsh_aliases\n"
-ln -sf "${DOTDIR}"/aliases .zsh_aliases
+maybe_run ln -sf "${DOTDIR}"/aliases .zsh_aliases
 
 # By convention, config files are kept in directories under ~/.config
 # Create links in ~/.config to directories in dotfiles/config.dir
 LINKDIR="${HOME}/dotfiles/config.dir"
 TARGETDIR="${HOME}/.config"
-mkdir -p "${TARGETDIR}"
+maybe_run mkdir -p "${TARGETDIR}"
 printf "# Creating links to %s in %s\n" \
     "${LINKDIR#"$HOME"/}" "${TARGETDIR#"$HOME"/}"
 cd "${TARGETDIR}" || exit
 for dir in $(eza -D "${LINKDIR}"); do
     printf "==> Linking %s\n" "${dir}"
-    rm -rf "${dir}"
-    ln -s "${LINKDIR}"/"${dir}" "${dir}"
+    maybe_run rm -rf "${dir}"
+    maybe_run ln -s "${LINKDIR}"/"${dir}" "${dir}"
 done
 
 # Special location required for fastfetch presets
 LINKDIR="${LINKDIR}/fastfetch"
 TARGETDIR="${HOME}/.local/share"
-mkdir -p "${TARGETDIR}"
+maybe_run mkdir -p "${TARGETDIR}"
 printf "# Creating link to %s in %s\n" \
     "${LINKDIR#"$HOME"/}" "${TARGETDIR#"$HOME"/}"
 printf "==> Linking fastfetch\n"
-ln -sf "${LINKDIR}" "${TARGETDIR}"
+maybe_run ln -sf "${LINKDIR}" "${TARGETDIR}"
