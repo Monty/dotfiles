183ada7d6b57b65283e0ecf811fd5bca5da86d7a
Monty Williams <monty.williams@gmail.com>
2025-11-18 22:33:49 -0800

    Enable verbose mode and dry run in installBinFiles
    Similar to install.sh

--- installBinFiles.sh
+++ installBinFiles.sh
@@ -1,6 +1,33 @@
 #!/usr/bin/env bash
-#
-# Create appropriate links in ~/bin to files in ~/dotfiles/bin.dir
+# Create links in ~/bin to files in ~/dotfiles/bin.dir
+
+# Prevent cascading or pipe failures
+set -euo pipefail
+
+# Check for either dry run or verbose option
+DRY_RUN=false
+VERBOSE=false
+case ${1-} in
+"") ;; # no argument → no dry run, no error
+-d | --dry-run)
+    DRY_RUN=true
+    printf "==> Starting dry run...\n"
+    ;;
+-v | --verbose)
+    VERBOSE=true
+    ;;
+*)
+    printf "[Warning] Ignoring invalid argument: '%s'\n" "$1" >&2
+    exit 1
+    ;;
+esac
+
+# Helper function to execute command unless in dry-run mode
+maybe_run() {
+    if [[ $DRY_RUN == false ]]; then
+        "$@"
+    fi
+}
 
 WARNING="\e[0;31m[Warning]\e[0m"
 
@@ -9,20 +36,23 @@ TARGETDIR="${HOME}/bin"
 
 cd "${TARGETDIR}" || exit
 
+[[ $VERBOSE == false ]] && printf "%s\n" \
+    "--- Note: There may be no links to create"
+
 printf "# Creating links to files in %s in %s\n" \
     "${LINKDIR#"$HOME"/}" "${TARGETDIR#"$HOME"/}"
 
 for file in $(fd -d 1 -t f); do
-    if [ -f "${LINKDIR}/${file}" ] && file "${LINKDIR}/${file}" |
+    if [[ -f "${LINKDIR}/${file}" ]] && file "${LINKDIR}/${file}" |
         grep -q 'script text executable'; then
         if ! cmp -s "${file}" "${LINKDIR}/${file}"; then
             printf "==> $WARNING Skipping %s as ~/%s is different\n" \
                 "${file}" "${LINKDIR#"$HOME"/}/${file}"
         else
-            printf "==> Linking %s\n" "${file}"
-            ln -sf "${LINKDIR}/${file}" "${file}"
+            printf "    Linking %s\n" "${file}"
+            maybe_run ln -sf "${LINKDIR}/${file}" "${file}"
         fi
     else
-        printf "==> Skipping %s\n" "${file}"
+        [[ $VERBOSE == true ]] && printf "    Ignoring %s\n" "${file}"
     fi
 done
