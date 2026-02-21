6254603dc352bf234321567f315c8fbbca795de8
Monty Williams <monty.williams@gmail.com>
2026-02-20 23:55:03 -0800

    Switch swiftformat.sh to use default  Input/Output

--- CotEditor.dir/swiftformat.sh
+++ CotEditor.dir/swiftformat.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-# %%%{CotEditorXInput=AllText}%%%
-# %%%{CotEditorXOutput=ReplaceAllText}%%%
 
-/Users/monty/.swiftly/bin/swiftformat --swiftversion 6.2.3 --disable docComments --stdinpath "$1"
+/Users/monty/.swiftly/bin/swiftformat --swiftversion 6.2.3 \
+    --disable docComments "$1"
