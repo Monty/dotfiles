65c41b1fc6cf4aacae281666b28a6042f6d9b5e2
Monty Williams <monty.williams@gmail.com>
2026-03-06 23:40:25 -0800

    Separate ESLint errors from Prettier errors
    Add CotEditor alert for ESLint errors

--- CotEditor.dir/prettier-eslint.sh
+++ CotEditor.dir/prettier-eslint.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 # Used to format JavaScript files in CotEditor
 
-"$HOME"/.volta/bin/eslint --fix --config \
-    "$HOME"/Projects/WhatsStreamingToday/eslint.config.mjs "$1"
+eslint_output=$("$HOME"/.volta/bin/eslint --fix --config \
+    "$HOME"/Projects/WhatsStreamingToday/eslint.config.mjs "$1" 2>&1)
+eslint_exit=$?
 
-if [[ $? -ne 1 ]]; then
-    "$HOME"/.volta/bin/prettier --write "$1"
+if [[ $eslint_exit -eq 1 || $eslint_exit -eq 2 ]]; then
+    echo "ESLint error (exit $eslint_exit):" >&2
+    echo "$eslint_output" >&2
+    osascript -e 'tell application "CotEditor"
+  display alert "ESLint error (exit '"$eslint_exit"')" message "'"$eslint_output"'"
+end tell'
+    exit 0
 fi
+
+"$HOME"/.volta/bin/prettier --write "$1"
