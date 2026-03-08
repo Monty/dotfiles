73c42d3c1b1f979fcdf9e58f3ad8294783003c68
Monty Williams <monty.williams@gmail.com>
2026-03-08 16:05:50 -0700

    Fix problematic shellcheck.sh

--- CotEditor.dir/shellcheck.sh
+++ CotEditor.dir/shellcheck.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Used to shellcheck Shell Scripts in CotEditor
 # %%%{CotEditorXInput=None}%%%
-# %%%{CotEditorXOutput=Discard}%%%
+# %%%{CotEditorXOutput=NewDocument}%%%
 
 SYNTAX=$(osascript -e '
     tell application "CotEditor"
@@ -14,14 +14,16 @@ if [[ $SYNTAX != "Shell Script" ]]; then
     tell application "CotEditor"
         display alert "shellcheck not available for '"$SYNTAX"' documents"
     end tell' >/dev/null
+    printf "==> Error: shellcheck not available for %s documents\n" "$SYNTAX"
     exit 0
 fi
 
 ERRORS=$("$HOME"/bin/shellcheck -s bash "$1")
 if [[ -n $ERRORS ]]; then
-    osascript -e '
-    tell application "CotEditor"
-        make new document
-        set contents of front document to "'"$ERRORS"'"
-    end tell' >/dev/null
+    printf "shellcheck %s\n" "$1"
+    printf "==> Found the following errors:\n"
+    printf "%s\n" "$ERRORS"
+else
+    printf "shellcheck %s\n" "$1"
+    printf "==> Found no errors\n"
 fi
