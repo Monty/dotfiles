775d2124d43758afcf60f4c07900890ddeaba8f7
Monty Williams <monty.williams@gmail.com>
2026-03-08 12:46:34 -0700

    Add shellcheck disable=SC2059 to prettier-eslint.sh

--- CotEditor.dir/prettier-eslint.sh
+++ CotEditor.dir/prettier-eslint.sh
@@ -5,6 +5,7 @@ eslint_output=$("$HOME"/.volta/bin/eslint --fix --config \
     "$HOME"/Projects/WhatsStreamingToday/eslint.config.mjs "$1" 2>&1)
 eslint_exit=$?
 
+# shellcheck disable=SC2059
 if [[ $eslint_exit -eq 1 || $eslint_exit -eq 2 ]]; then
     printf "ESLint error (exit $eslint_exit):\n" >&2
     printf "$eslint_output\n" >&2
